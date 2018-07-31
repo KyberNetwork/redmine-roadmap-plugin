55309a1292bff70ba4edc5f8bf15402cf6fae2d0
Vinh Nguyen <nguyenducvinh92@gmail.com>
2018-07-31 13:52:14 +0700

    Add filter for Roadmap page

--- init.rb
+++ init.rb
@@ -1,6 +1,11 @@
+require 'versions_controller_patch'
 Redmine::Plugin.register :roadmap do
   name 'Roadmap plugin'
   author 'Vinh Ng'
   description 'This is a plugin for roadmap of Redmine'
   version '0.0.1'
+
+  Rails.configuration.to_prepare do 
+    VersionsController.send(:include, VersionList::VersionsControllerPatch)
+  end
 end
