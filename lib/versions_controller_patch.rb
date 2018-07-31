5c9663375e3cee9c776974a78b5eede55eb0c99d
Vinh Nguyen <nguyenducvinh92@gmail.com>
2018-07-31 18:42:56 +0700

    Update roadmap

--- lib/versions_controller_patch.rb
+++ lib/versions_controller_patch.rb
@@ -61,7 +61,7 @@ module VersionList
         end
 
         def load_issue_statuses
-          @open_status_ids = IssueStatus.where("name LIKE 'new' OR name LIKE 'in progress' OR name LIKE 'reopen'")
+          @open_status_ids = IssueStatus.where("LOWER(name) = 'new' OR LOWER(name) = 'in progress' OR LOWER(name) = 'reopen'")
             .pluck(:id).collect { |id| id.to_i.to_s }
           @resolved_status_id = IssueStatus.where("LOWER(name) = 'resolved'").first.try :id
           @deployed_status_id = IssueStatus.where("LOWER(name) = 'deployed'").first.try :id
